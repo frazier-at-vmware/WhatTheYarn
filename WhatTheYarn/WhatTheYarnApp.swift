18493733acc8717de6640dcbc761b0ae07857fbf
Frazier Smith <frazier@vfrazier.net>
2024-02-05 10:47:00 -0500

    Initial Commit

---
+++ WhatTheYarn/WhatTheYarnApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatTheYarnApp.swift
+//  WhatTheYarn
+//
+//  Created by Frazier Smith on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct WhatTheYarnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
